ccc830627d9e15bc1d27d148cd4b4799b1bd233c
gabrieladutra <gabriela_ddutra@outlook.com>
2024-07-21 16:13:49 -0300

    Added switch method

--- public/script.js
+++ public/script.js
@@ -1,71 +1,29 @@
-// window.onload = function (){
-//     document.addEventListener('DOMContentLoaded', (event) => {
-//         document.getElementById('btn-br').onclick = changeLanguage();
-//     });
-//     document.addEventListener('DOMContentLoaded', (event) => {
-//         document.getElementById('btn-en').onclick = changeLanguage();
-//     });
-
-//     function changeLanguage(){
-//         alert("Clicou")
-//     }
-
-// }
-// inserir click
-// let btn = document.getElementById("btn-en")
-
-// console.log("JesusS");
-
-// btn.addEventListener("click", function() {
-
-//   console.log("clicou");
-//   alert("hello")
-
-// });
-
-
 window.onload = async () => {
-    // Carregando arquivo de tradução e realizando substituições
     const arquivo = await fetch("languages.json")
-    const data = await arquivo.json();
-    console.log('traducao', data)
+    const data = await arquivo.json()
     let html = document.body.innerHTML
-    function mudarIdioma(idioma) {
+
+    function switchlanguage() {
+        let btnEn = document.getElementById("btn-en")
+        let btnPt = document.getElementById("btn-br")
+
+        btnEn.onclick = () => {
+            replaceLanguage("en")
+        };
+
+        btnPt.onclick = () => {
+            replaceLanguage("pt")
+        };
+    }
+
+    function replaceLanguage(idioma) {
         let language = data[idioma]
         document.body.innerHTML = html.replaceAll('{{projectsTitle}}', language.projectsTitle)
         document.body.innerHTML = document.body.innerHTML.replaceAll('{{projectsList}}', language.projectsList)
-        document.body.innerHTML = document.body.innerHTML.replaceAll('{{aboutMe}}', language.aboutMe)
+        document.body.innerHTML = document.body.innerHTML.replaceAll('{{aboutMe}}', language.aboutMe);
+        switchlanguage()
     }
-    mudarIdioma("pt")
-    
-    let btnEn = document.getElementById("btn-en");
-    console.log(btnEn)
-    btnEn.onclick = () => {
-        console.log("Botão inglês")
-        mudarIdioma("en")
-        console.log(html)
-    }
-    console.log(html)
-    let btnPt = document.getElementById("btn-br");
-    console.log(btnPt)
-    console.log(btnPt)
-    btnPt.onclick = () => {
-        console.log("Clicou em Portugues")
-        mudarIdioma("pt")
-    }
+
+    replaceLanguage("pt")
+    switchlanguage()
 }
-
-
-
-// let language = data.pt // To-do : Mudar a lógica para substituir pt por en ao clicar nos botões
-// document.body.innerHTML = document.body.innerHTML.replaceAll('{{projectsTitle}}', language.projectsTitle)
-// document.body.innerHTML = document.body.innerHTML.replaceAll('{{projectsList}}', language.projectsList)
-// document.body.innerHTML = document.body.innerHTML.replaceAll('{{aboutMe}}', language.aboutMe)
-
-
-
-
-
-
-
-
