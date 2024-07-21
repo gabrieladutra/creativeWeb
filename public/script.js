69112fdecc25cd6efa56f14bcbb02d968dd6099b
gabrieladutra <gabriela_ddutra@outlook.com>
2024-07-20 23:31:38 -0300

    Added first possibility of replacement

--- public/script.js
+++ public/script.js
@@ -1,23 +1,69 @@
-function clicar() {
-    let a = document.getElementById('btn-br');
-    a.innerText = "Clicou"; 
-    alert("Clicou");
+// window.onload = function (){
+//     document.addEventListener('DOMContentLoaded', (event) => {
+//         document.getElementById('btn-br').onclick = changeLanguage();
+//     });
+//     document.addEventListener('DOMContentLoaded', (event) => {
+//         document.getElementById('btn-en').onclick = changeLanguage();
+//     });
+
+//     function changeLanguage(){
+//         alert("Clicou")
+//     }
+
+// }
+// inserir click
+// let btn = document.getElementById("btn-en")
+
+// console.log("JesusS");
+
+// btn.addEventListener("click", function() {
+
+//   console.log("clicou");
+//   alert("hello")
+
+// });
+
+
+window.onload = async () => {
+    // Carregando arquivo de tradução e realizando substituições
+    const arquivo = await fetch("languages.json")
+    const data = await arquivo.json();
+    console.log('traducao', data)
+    let html = document.body.innerHTML
+    function mudarIdioma(idioma) {
+        let language = data[idioma]
+        document.body.innerHTML = html.replaceAll('{{projectsTitle}}', language.projectsTitle)
+        document.body.innerHTML = document.body.innerHTML.replaceAll('{{projectsList}}', language.projectsList)
+        document.body.innerHTML = document.body.innerHTML.replaceAll('{{aboutMe}}', language.aboutMe)
+    }
+    mudarIdioma("pt")
+    
+    let btnEn = document.getElementById("btn-en");
+    console.log(btnEn)
+    btnEn.onclick = () => {
+        console.log("Botão inglês")
+        mudarIdioma("en")
+        console.log(html)
+    }
+    console.log(html)
+    let btnPt = document.getElementById("btn-br");
+    console.log(btnPt)
+    console.log(btnPt)
+    btnPt.onclick = () => {
+        console.log("Clicou em Portugues")
+        mudarIdioma("pt")
+    }
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('btn-br').onclick = clicar;
-});
 
 
-const arquivo = await fetch("languages.json")
-const data = await arquivo.json();
-console.log(data)
+// let language = data.pt // To-do : Mudar a lógica para substituir pt por en ao clicar nos botões
+// document.body.innerHTML = document.body.innerHTML.replaceAll('{{projectsTitle}}', language.projectsTitle)
+// document.body.innerHTML = document.body.innerHTML.replaceAll('{{projectsList}}', language.projectsList)
+// document.body.innerHTML = document.body.innerHTML.replaceAll('{{aboutMe}}', language.aboutMe)
 
 
-let language = data.pt // To-do : Mudar a lógica para substituir pt por en ao clicar nos botões
-document.body.innerHTML = document.body.innerHTML.replaceAll('{{projectsTitle}}', language.projectsTitle)
-document.body.innerHTML = document.body.innerHTML.replaceAll('{{projectsList}}', language.projectsList)
-document.body.innerHTML = document.body.innerHTML.replaceAll('{{aboutMe}}', language.aboutMe)
+
 
 
 
