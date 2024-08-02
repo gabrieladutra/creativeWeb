887421abb60aff990af0460ff6fd34db1b240f93
gabrieladutra <gabriela_ddutra@outlook.com>
2024-08-01 21:42:21 -0300

    Added third possibility

--- public/script.js
+++ public/script.js
@@ -1,27 +1,45 @@
 window.onload = async () => {
     const arquivo = await fetch("languages.json")
     const data = await arquivo.json()
-    let html = document.body.innerHTML
 
-    function switchLanguage() {
-        let btnEn = document.getElementById("btn-en")
-        let btnPt = document.getElementById("btn-br")
+    let body = funcaoRecursiva(document.body)
+     let currentLanguage = localStorage.getItem('language') || 'pt'
+    for (let i = 0; i < body.length; i++) {
+        let elementoAtual = body[i]
+        if (elementoAtual.innerText == '') {
+            continue
+        }
+        elementoAtual.innerText = Mustache.render(elementoAtual.innerText, data[currentLanguage])
 
-        btnEn.onclick = () => {
-            replaceLanguage("en")
-        };
+    }
+    let btnEn = document.getElementById("btn-en")
+    let btnPt = document.getElementById("btn-br")
 
-        btnPt.onclick = () => {
-            replaceLanguage("pt")
-        };
+    btnEn.onclick = () => {
+        localStorage.setItem('language', 'en')
+        window.location.reload()
+    };
+
+    btnPt.onclick = () => {
+        localStorage.setItem('language', 'pt')
+        window.location.reload()
     }
 
-    function replaceLanguage(idioma) {
-        let language = data[idioma]
-        document.body.innerHTML = Mustache.render(html, language)
-        switchLanguage()
-    }
-
-    replaceLanguage("pt")
-    switchLanguage()
 }
+
+function funcaoRecursiva(elemento) {
+    let elementos = []
+    if (elemento.children.length == 0) {
+        return [elemento]
+    }
+    for (let i = 0; i < elemento.children.length; i++) {
+        if (elemento.children[i].length == 0) {
+            elementos.push(elemento.children[i])
+        } else {
+            let filhos = funcaoRecursiva(elemento.children[i])
+            elementos = elementos.concat(filhos)
+        }
+    }
+    return elementos
+}
+window.funcaoRecursiva = funcaoRecursiva
